9d38a43abb2673901e9176f59eabc2d1382aef2d
unknown <Eugene Hung@Eugene_Hung.ggec.gd>
2020-10-26 11:06:43 +0800

    20201026

--- eugene_test.c
+++ eugene_test.c
@@ -2,22 +2,27 @@
 
 int main()
 {
-    struct bs
+    FILE *fp;
+    char filename[40]  ;
+    int i,j ;
+    float da[6][10] = {0} ;
+    printf("filename : ");
+    gets(filename);
+    fp=fopen("as.csv","r");
+    fseek(fp, 5L, SEEK_SET);   // 从文件第二行开始读取
+    for(i = 0 ;i < 6 ; i++)
     {
-        unsigned int a : 1;
-        unsigned int b : 3;
-        unsigned int c : 4;
-    } bit, *pbit;
+        for(j = 0 ;j < 10 ; j++)
+		{
+			fscanf(fp,"%f",&da[i][j]);
+			fseek(fp, 1L, SEEK_CUR);   /*fp指针从当前位置向后移动*/
+		}
+    }       
+	for(i = 0 ;i < 6 ; i++)
+    {
+        printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t\n",da[i][0],
+         da[i][1],da[i][2],da[i][3],da[i][4],da[i][5],da[i][6],da[i][7],da[i][8],da[i][9]);
+    } 
 
-    bit.a = 1;
-    bit.b = 4;
-    bit.c = 15;
-    bit.b = bit.b | bit.a;
-    printf("%x,%x,%x \n", bit.a, bit.b, bit.c);
-    pbit = &bit;
-    pbit->a = 0;
-    pbit->b &= 3;
-    pbit->c |= 1;
-    printf("%d,%d,%d \n", pbit->a, pbit->b, pbit->c);
     system("pause");
 }
