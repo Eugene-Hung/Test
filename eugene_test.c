c7322b29e97c86cc85fb0a3e1d2d1a213277c133
Eugene Hung <Eugene Hung@Eugene_Hung.ggec.gd>
2020-10-28 08:56:08 +0800

    20201020

--- eugene_test.c
+++ eugene_test.c
@@ -2,27 +2,7 @@
 
 int main()
 {
-    FILE *fp;
-    char filename[40]  ;
-    int i,j ;
-    float da[6][10] = {0} ;
-    printf("filename : ");
-    gets(filename);
-    fp=fopen("as.csv","r");
-    fseek(fp, 5L, SEEK_SET);   // 从文件第二行开始读取
-    for(i = 0 ;i < 6 ; i++)
-    {
-        for(j = 0 ;j < 10 ; j++)
-		{
-			fscanf(fp,"%f",&da[i][j]);
-			fseek(fp, 1L, SEEK_CUR);   /*fp指针从当前位置向后移动*/
-		}
-    }       
-	for(i = 0 ;i < 6 ; i++)
-    {
-        printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t\n",da[i][0],
-         da[i][1],da[i][2],da[i][3],da[i][4],da[i][5],da[i][6],da[i][7],da[i][8],da[i][9]);
-    } 
-
+    unsigned short a = 1;
+    printf(" %x \n",a);
     system("pause");
 }
